91ebf8d39e1393b6e606bde46584b38fbced1336
Thomas Heim <wearedreamin@gmail.com>
2024-06-19 15:18:13 +0100

    update yul✅

--- test/v1/HorseStoreYul.sol
+++ test/v1/HorseStoreYul.sol
@@ -1,11 +1,10 @@
 // SPDX-License-Identifier: SEE LICENSE IN LICENSE
 pragma solidity ^0.8.20;
 
-import {Base_TestV1} from "./Base_TestV1.sol";
+import {Base_TestV1, IHorseStore} from "./Base_TestV1.sol";
 import {HorseStore} from "../../src/horseStorev1/HorseStoreYul.sol";
 
 contract HorseStoreSolc is Base_TestV1 {
     function setUp() public override {
-        horseStore = new HorseStore();
-    }
+        horseStore = IHorseStore(address(new HorseStoreYul))
 }
