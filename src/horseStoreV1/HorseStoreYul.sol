cbce6141d2cec28cb0fe0c6292c8c5c64a490e1f
Thomas Heim <wearedreamin@gmail.com>
2024-06-19 15:04:45 +0100

    update yul✅

---
+++ src/horseStoreV1/HorseStoreYul.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: SEE LICENSE IN LICENSE
+pragma solidity ^0.8.20;
+
+contract HorseStore {
+    uint256 numberOfHorses;
+
+    function updateHorseNumber(uint256 newNumberOfHorses) external {
+        // numberOfHorses = newNumberOfHorses;
+        assembly {
+            sstore(numberOfHorses.slot, newNumberOfHorses)
+        }
+    }
+
+    function readNumberOfHorses() external view returns (uint256) {
+        assembly {
+            let num := sload(numberOfHorses.slot)
+        }
+    }
+}
