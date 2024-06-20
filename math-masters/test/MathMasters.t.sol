5d999b8661f85bf4ac645c74f808eca7739b8e56
Thomas Heim <wearedreamin@gmail.com>
2024-06-20 10:52:34 +0100

    update math-masters tests✅

--- math-masters/test/MathMasters.t.sol
+++ math-masters/test/MathMasters.t.sol
@@ -11,6 +11,10 @@ contract MathMastersTest is Base_Test {
         assertEq(MathMasters.mulWad(369, 271), 0);
     }
 
+    function testMulRevert() public {
+        MathMasters.mulWad(type(uint256).max, type(uint256).max);
+    }
+
     function testMulWadFuzz(uint256 x, uint256 y) public pure {
         // Ignore cases where x * y overflows.
         unchecked {
